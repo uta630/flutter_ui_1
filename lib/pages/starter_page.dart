bb856e662c0fb03a7d4c43b4a2ad0c84f299bc1b
uta630 <usuitkc.630@gmail.com>
2021-10-17 17:40:54 +0900

    フォント追加

---
+++ lib/pages/starter_page.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+
+class StarterPage extends StatefulWidget {
+  const StarterPage({Key? key}) : super(key: key);
+
+  @override
+  _StarterPageState createState() => _StarterPageState();
+}
+
+class _StarterPageState extends State<StarterPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/cheers-picjumbo-com.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.bottomCenter,
+              colors: [
+                Colors.black.withOpacity(.9),
+                Colors.black.withOpacity(.8),
+                Colors.black.withOpacity(.2),
+              ],
+            ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  'あなたの思い描く食卓を',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 50,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                const Text(
+                  "さまざまな料理をご用意しています。\n是非お楽しみください",
+                  style: TextStyle(
+                    color: Colors.white,
+                    height: 1.4,
+                    fontSize: 18,
+                  ),
+                ),
+                const SizedBox(height: 100),
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    gradient: const LinearGradient(
+                      colors: [
+                        Colors.yellow,
+                        Colors.orange,
+                      ],
+                    ),
+                  ),
+                  child: const MaterialButton(
+                    minWidth: double.infinity,
+                    onPressed: null,
+                    child: Text(
+                      "Start",
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 30),
+                const Align(
+                  child: Text(
+                    "来月１日より随時お届けします。",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 14,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 30),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
